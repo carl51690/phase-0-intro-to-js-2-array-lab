b8854879a8055b2201ef3170803db2c37c014f14
Carl Adams <carl51690@gmail.com>
2023-09-29 09:17:20 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    const copycats = [...cats]
+    copycats.push(name)
+    return copycats
+}
+function prependCat(name){
+    const copycats = [...cats]
+    copycats.unshift(name)
+    return copycats
+}
+function removeLastCat(){
+    const copycats = [...cats]
+    copycats.pop()
+    return copycats
+}
+function removeFirstCat(){
+    const copycats = [...cats]
+    copycats.shift()
+    return copycats
+}
